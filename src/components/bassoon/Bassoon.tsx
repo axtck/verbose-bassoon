4c73485b8887fbee19662c64eba2811a89b8b0cf
Alexander Tack <alexander.tack@student.vives.be>
2021-02-23 12:11:51 +0100

    Add verbose and bassoon components

---
+++ src/components/bassoon/Bassoon.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react';
+
+interface BassoonProps { };
+
+const Bassoon: FunctionComponent<BassoonProps> = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Bassoon;
---
+++ src/components/verbose/Verbose.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react';
+
+interface VerboseProps { };
+
+const Verbose: FunctionComponent<VerboseProps> = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Verbose
